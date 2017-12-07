8875bed376d7cafb6dfb6afc5d225a6460ebae26
zibinmei <zibinmei@outlook.com>
2017-12-06 21:27:22 -0700

    byte check

--- convole.c
+++ convole.c
@@ -5,28 +5,30 @@
 
 
 void fileReader(char *fname){
-    //https://stackoverflow.com/questions/22059189/read-a-file-as-byte-array
+//https://stackoverflow.com/questions/26370012/read-bytes-from-file-and-store-in-array-c
     FILE *fptr;
     char *buffer;
     long filelen;
 
     fptr = fopen(fname,"rb");
     if (fptr == NULL){
+        printf("ERROR: Unable to open file\n");
         exit(0);
     }
 
 
     fseek(fptr, 0, SEEK_END);
     filelen = ftell(fptr);
+    char bytes[filelen];
     rewind(fptr);
 
     buffer = (char *)malloc((filelen+1)*sizeof(char));
-    fread(buffer, filelen, 1, fptr);
+    fread(bytes, filelen, 1, fptr);
     fclose(fptr);
 
 
 
-    printf("%x ", buffer);
+    printf("%x ", bytes[1]);
 
 
 }
